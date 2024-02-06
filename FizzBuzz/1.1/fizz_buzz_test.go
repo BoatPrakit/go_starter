ce4e2fe92567a285fd0c14f9780c2c56ebaa175a
prakit.w <prakit.w@arise.tech>
2024-02-06 22:05:19 +0700

    should be return Fizz when input 6

--- FizzBuzz/1.1/fizz_buzz_test.go
+++ FizzBuzz/1.1/fizz_buzz_test.go
@@ -58,4 +58,14 @@ func TestFizzBuzz(t *testing.T) {
 		}
 	})
 
+	t.Run("should be return Fizz when input 6", func(t *testing.T) {
+		input := 6
+		want := "Fizz"
+
+		result := FizzBuzz(input)
+
+		if result != want {
+			t.Errorf("FizzBuzz should be return %v when input %v", want, input)
+		}
+	})
 }
