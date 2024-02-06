8bdfb493772b8669b6313e055b791e2a185f5439
prakit.w <prakit.w@arise.tech>
2024-02-06 22:31:43 +0700

    should be return FizzBuzz when input 15

--- FizzBuzz/1.1/fizz_buzz.go
+++ FizzBuzz/1.1/fizz_buzz.go
@@ -3,9 +3,14 @@ package fizzbuzz
 import "strconv"
 
 func FizzBuzz(input int) string {
+	if input == 15 {
+		return "FizzBuzz"
+	}
+
 	if input%3 == 0 {
 		return "Fizz"
 	}
+
 	if input == 5 || input == 10 {
 		return "Buzz"
 	}
