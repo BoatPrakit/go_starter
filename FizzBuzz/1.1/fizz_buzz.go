1f8ae04960812faf329c1b01206365900e9c0e29
prakit.w <prakit.w@arise.tech>
2024-02-08 12:50:48 +0700

    [2] should be return 1 when input 1

--- FizzBuzz/1.1/fizz_buzz.go
+++ FizzBuzz/1.1/fizz_buzz.go
@@ -1,27 +1,6 @@
-package fizzbuzz
-
-import "strconv"
+package fizz_buzz
 
 func FizzBuzz(input int) string {
-	if isFizz(input) && isBuzz(input) {
-		return "FizzBuzz"
-	}
-
-	if isFizz(input) {
-		return "Fizz"
-	}
-
-	if isBuzz(input) {
-		return "Buzz"
-	}
-
-	return strconv.Itoa(input)
+	return "1"
 }
 
-func isFizz(input int) bool {
-	return input%3 == 0
-}
-
-func isBuzz(input int) bool {
-	return input%5 == 0
-}
