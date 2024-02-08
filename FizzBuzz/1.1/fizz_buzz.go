c2cf1a5b36db35fb7770d88304d808cd980acae0
prakit.w <prakit.w@arise.tech>
2024-02-08 12:51:40 +0700

    [2] should be return 2 when input 2

--- FizzBuzz/1.1/fizz_buzz.go
+++ FizzBuzz/1.1/fizz_buzz.go
@@ -1,6 +1,8 @@
 package fizz_buzz
 
 func FizzBuzz(input int) string {
+	if input == 2 {
+		return "2"
+	}
 	return "1"
 }
-
--- FizzBuzz/1.1/fizz_buzz_test.go
+++ FizzBuzz/1.1/fizz_buzz_test.go
@@ -13,4 +13,15 @@ func TestFizzBuzz(t *testing.T) {
 			t.Errorf("FizzBuzz should return %v when input %v", want, input)
 		}
 	})
+
+	t.Run("should be return 2 when input 2", func(t *testing.T) {
+		input := 2
+		want := "2"
+
+		result := FizzBuzz(input)
+
+		if result != want {
+			t.Errorf("FizzBuzz should return %v when input %v", want, input)
+		}
+	})
 }
