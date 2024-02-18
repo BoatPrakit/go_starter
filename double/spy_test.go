13cd14ebe13ec8981e80d1219b3784fc31f3dab6
prakit.w <prakit.w@arise.tech>
2024-02-18 14:44:15 +0700

    [Boat] add spy test

---
+++ double/spy_test.go
@@ -0,0 +1,30 @@
+package double
+
+import "testing"
+
+type SpySearcher struct {
+	methodCalled bool
+}
+
+func (s *SpySearcher) Search(p []*Person, fn string, ln string) *Person {
+	s.methodCalled = true
+
+	return &Person{}
+}
+
+func TestMethodShouldBeCalled(t *testing.T) {
+	want := true
+
+	spy := &SpySearcher{}
+	phoneBook := &PhoneBook{}
+
+	_, err := phoneBook.Find(spy, "f", "s")
+
+	if err != nil {
+		t.Errorf("should not error %v", err)
+	}
+
+	if spy.methodCalled != want {
+		t.Errorf("want %v but got %v", want, spy.methodCalled)
+	}
+}
