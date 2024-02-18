758acf9179096aa961a8de534e6fb4dc379732b6
prakit.w <prakit.w@arise.tech>
2024-02-18 21:54:28 +0700

    FirstNonRepeating should return empty string when input aaaaaa

--- codewars/first_non_repeating_character/first_non_repeat_character_test.go
+++ codewars/first_non_repeating_character/first_non_repeat_character_test.go
@@ -3,9 +3,19 @@ package codewars
 import "testing"
 
 func TestFirstNonRepeating(t *testing.T) {
-	t.Run("" , func(t *testing.T) {
+	t.Run("should return empty string when input empty string", func(t *testing.T) {
 		want := ""
 		result := FirstNonRepeating("")
+
+		if result != want {
+			t.Errorf("want %v but got %v", want, result)
+		}
+	})
+
+	t.Run("should return empty string when input aaaaaa", func(t *testing.T) {
+		want := ""
+		input := "aaaaaa"
+		result := FirstNonRepeating(input)
 
 		if result != want {
 			t.Errorf("want %v but got %v", want, result)
