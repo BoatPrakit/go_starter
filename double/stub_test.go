027626037b1c1a30d7efbbed427af3d0aeeb248f
prakit.w <prakit.w@arise.tech>
2024-02-18 14:20:28 +0700

    [Boat] add stub test

---
+++ double/stub_test.go
@@ -0,0 +1,31 @@
+package double
+
+import "testing"
+
+type StubSearcher struct {
+	phone string
+}
+
+func (s *StubSearcher) Search(p []Person, firstName string, lastName string) *Person {
+	return &Person{
+		FirstName:   "John",
+		LastName:    "Doe",
+		PhoneNumber: "+66 123456789",
+	}
+}
+
+func TestFoundPhoneNumber(t *testing.T) {
+	phoneBook := &PhoneBook{}
+
+	want := "+66 123456789"
+
+	phoneNumber, err := phoneBook.Find(&StubSearcher{want}, "John", "Doe")
+
+	if err != nil {
+		t.Errorf("should not be error %v", err)
+	}
+
+	if phoneNumber != want {
+		t.Errorf("want %v but got %v", want, phoneNumber)
+	}
+}
